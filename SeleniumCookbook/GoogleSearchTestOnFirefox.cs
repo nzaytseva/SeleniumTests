3adbb157ae7a916c09ce74fc493a9eba6e34403e
Natalia Yukhimenko <nzaytseva@corepartners.ru>
2016-08-01 15:12:04 +0300

    1st commit

---
+++ SeleniumCookbook/GoogleSearchTestOnFirefox.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support;
+
+namespace SeleniumCookbook
+{
+    [TestClass]
+    public class GoogleSearchTestOnFirefox
+    {
+        FirefoxDriver _driver;
+        string _url = "https://www.google.ru/";
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            _driver = new FirefoxDriver();
+            _driver.Navigate().GoToUrl(_url);
+        }
+
+        [TestMethod]
+        public void TestGoogleSearch()
+        {
+            FirefoxWebElement searchString = (FirefoxWebElement)_driver.FindElement(OpenQA.Selenium.By.Id("lst-ib"));
+
+            try
+            {
+                searchString.SendKeys("shameless us");
+                searchString.Submit();
+            }
+           catch (OpenQA.Selenium.NoSuchElementException noSuchElementException)
+            {
+              
+                
+            }
+  
+        }
+
+        [TestCleanup]
+        public void CloseBrowser()
+        {
+            _driver.Quit();
+        }
+        
+    }
+}
