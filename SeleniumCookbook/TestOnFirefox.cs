3659490c1f4f145501e1c1311908c0ac0d79c358
Natalie <Natalie@NATALIE-PC>
2016-08-03 21:27:03 +0300

    added some methods

--- SeleniumCookbook/TestOnFirefox.cs
+++ SeleniumCookbook/TestOnFirefox.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support;
@@ -36,34 +37,85 @@ namespace SeleniumCookbook
             }
         }
 
-        /*  public void FindElementByXPath()
+        /*
+        [TestMethod]
+          public void FindElementByXPath()
           {
           }
           */
 
+        [TestMethod]
         public void FindElementsByTagName()
         {
-            string tagName = "input";
-          //  IReadOnlyCollection <FirefoxWebElement> textFields = _driver.FindElements(OpenQA.Selenium.By.TagName(tagName));
-           
+            string tagName = "button";
+            // Find all input elements
+            IReadOnlyCollection <OpenQA.Selenium.IWebElement> buttons = _driver.FindElements(OpenQA.Selenium.By.TagName(tagName));
+            Debug.Print("There were {0} elements found", buttons.Count);
         }
 
-// try FindElements
-
-
-// Name 
-// ID - done in FindElementById()
-// TagName
-// Class
-// LinkText
-// PartialLinkText
-// XPath
-// CSS
-[TestCleanup]
-        public void CloseBrowser()
+        [TestMethod]
+        public void FindElemetByClassName()
         {
-            _driver.Quit();
+            string swapButtonClassName = "y-button_islet-rasp-swap";
+            string destinationFieldName = "toName";
+            FirefoxWebElement swapButton;
+            FirefoxWebElement destinationTextField;
+
+            // find and fill "destination" text field
+            try
+            {
+                destinationTextField = (FirefoxWebElement)_driver.FindElementByName(destinationFieldName);
+                destinationTextField.SendKeys("Калуга");
+            }
+            catch (OpenQA.Selenium.NoSuchElementException noSuchElementException)
+            {
+                Debug.Print("The element {0} not found", destinationFieldName);
+            }
+
+            // find and click "swap cities" button
+            try
+            {
+                swapButton = (FirefoxWebElement)_driver.FindElementByClassName(swapButtonClassName);
+                swapButton.Click();
+            }
+            catch (OpenQA.Selenium.NoSuchElementException noSuchElementException)
+            {
+                Debug.Print("The element of the class {0} not found", swapButtonClassName);
+            }
         }
-        
+
+        [TestMethod]
+        public void FindElementByLinkText()
+        {
+            string linkText = "Лискинское";
+            FirefoxWebElement link;
+
+            // find and click the link "Вяземское"
+            try
+            {
+                link = (FirefoxWebElement)_driver.FindElementByLinkText(linkText);
+                link.Click();
+            }
+            catch (OpenQA.Selenium.NoSuchElementException noSuchElementException)
+            {
+                Debug.Print("The link with the text {0} not found", linkText);
+            }
+        }
+
+        // Name - done in FindElemetByClassName()
+        // ID - done in FindElementById()
+        // TagName - done in FindElementsByTagName()
+        // Class - done in FindElemetByClassName()
+        // LinkText - done in FindElementByLinkText()
+        // PartialLinkText - won't be done
+        // XPath
+        // CSS
+
+        /*  [TestCleanup]
+          public void CloseBrowser()
+          {
+              _driver.Quit();
+          }
+          */
     }
 }
