e8f42350b205a350d7ebc0adf56bd22d932968c7
Natalie <Natalie@NATALIE-PC>
2016-08-09 20:39:08 +0300

    finished the task

--- SeleniumCookbook/Task.cs
+++ SeleniumCookbook/Task.cs
@@ -10,7 +10,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SeleniumCookbook
 {
-    class Task
+    [TestClass]
+    public class Task
     {
         FirefoxDriver _driver;
         string _url = "https://rasp.yandex.ru/";
@@ -22,18 +23,75 @@ namespace SeleniumCookbook
             _driver.Navigate().GoToUrl(_url);
         }
 
-        FirefoxWebElement FindDepartureField()
+        FirefoxWebElement GetDepartureField()
         {
             string fieldName = "fromName";
             return (FirefoxWebElement)_driver.FindElementByName(fieldName);   
         }
 
+        FirefoxWebElement GetArrivalField()
+        {
+            string fieldName = "toName";
+            return (FirefoxWebElement)_driver.FindElementByName(fieldName);
+        }
+
+        FirefoxWebElement GetDateField()
+        {
+            string fieldName = "when";
+            return (FirefoxWebElement)_driver.FindElementByName(fieldName);
+        }
+
+        FirefoxWebElement GetSearchButton()
+        {
+            string buttonClassName = "y-button_islet-rasp-search";
+            return (FirefoxWebElement)_driver.FindElementByClassName(buttonClassName);
+        }
+
+        FirefoxWebElement GetTomorrowButton()
+        {
+            //string cssSelector = "button[textContent='завтра']";
+            //return (FirefoxWebElement)_driver.FindElementByCssSelector(cssSelector);
+
+            string xPath = "//button[contains(text(),'завтра')]";
+            return (FirefoxWebElement)_driver.FindElementByXPath(xPath);
+        }
+
+        FirefoxWebElement GetSuburbanLabel()
+        {
+            string value = "input[value='suburban'][type='radio']";
+            return (FirefoxWebElement)_driver.FindElementByCssSelector(value);
+        }
+
         [TestMethod]
         public void FindSchedule()
         {
+            string requestedDeparture = "Калуга";
+            string requestedArrival = "Москва";
+            //string requestedDate = "13.08.2016";
             try
             {
-                FindDepartureField();
+                FirefoxWebElement departureField = GetDepartureField();
+                departureField.Clear();
+                departureField.SendKeys(requestedDeparture);
+
+                FirefoxWebElement arrivalField = GetArrivalField();
+                arrivalField.Clear();
+                arrivalField.SendKeys(requestedArrival);
+
+                /*
+                FirefoxWebElement dateField = GetDateField();
+                dateField.Clear();
+                dateField.SendKeys(requestedDate);
+                */
+
+                FirefoxWebElement suburbanLabel = GetSuburbanLabel();
+                suburbanLabel.Click();
+
+                FirefoxWebElement tomorrowButton = GetTomorrowButton();
+                tomorrowButton.Click();
+
+                FirefoxWebElement searchButton = GetSearchButton();
+                searchButton.Click();
             }
             catch (OpenQA.Selenium.NoSuchElementException exception)
             {
