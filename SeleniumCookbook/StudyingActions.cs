c8ee9a5108032842df4d7d8b70e732e6486f7c31
Natalie <Natalie@NATALIE-PC>
2016-08-25 21:14:27 +0300

    hh

---
+++ SeleniumCookbook/StudyingActions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System.Diagnostics;
+
+namespace SeleniumCookbook
+{
+    [TestClass]
+    public class StudyingActions
+    {
+        FirefoxDriver _driver;
+        string _url = "http://www.zozhnik.ru/";
+
+        [TestInitialize]
+        public void GoToUrl()
+        {
+            _driver = new FirefoxDriver();
+            _driver.Navigate().GoToUrl(_url);
+        }
+
+       /* [TestMethod]
+        public void SelectSeveralElements()
+        {
+            Actions builder = new Actions(_driver);
+            builder.KeyDown(OpenQA.Selenium.Keys.Control)
+                .Click()
+                .Click()
+                .Click()
+                .KeyUp(OpenQA.Selenium.Keys.Control);
+
+            // Generate the composite action.
+            builder.Perform();
+        }
+        */
+        [TestMethod]
+        public void PrintTextInUpperCase()
+        {
+            string text = "кбжу";
+            string searchStringId = "s";
+            string searchStringName = "s";
+
+            FirefoxWebElement searchString = (FirefoxWebElement)_driver.FindElementById(searchStringId);
+            Assert.AreEqual(searchString.GetAttribute("name"), searchStringName);
+
+            searchString.SendKeys(OpenQA.Selenium.Keys.Shift + text + OpenQA.Selenium.Keys.Enter);
+        }
+
+           
+        
+    }
+}
