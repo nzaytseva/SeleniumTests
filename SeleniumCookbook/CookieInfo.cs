7a0b747d3e1e9aa73e47120414885ec80329c17f
Natalia Yukhimenko <nzaytseva@corepartners.ru>
2016-09-06 13:02:32 +0300

    logged on. started writing into the file

--- SeleniumCookbook/CookieInfo.cs
+++ SeleniumCookbook/CookieInfo.cs
@@ -1,19 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Internal;
 using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 
 namespace SeleniumCookbook
 {
-    public class CookieInfo
+    public static class CookieInfo
     {
         static FirefoxDriver driver;
+        static string url = "http://klgw-019.corepartners.local:10022/client/#/auth/logon";
 
-        public static void InitializeDriver()
+        static void InitializeDriver()
         {
             FirefoxBinary firefoxPath = new FirefoxBinary("C:\\program files\\Mozilla Firefox\\firefox.exe");
             FirefoxProfile firefoxProfile = new FirefoxProfile();
@@ -22,13 +22,32 @@ namespace SeleniumCookbook
 
         public static void PmxAuthorize(string login, string password)
         {
-            string url = "http://klgw-019.corepartners.local:10022/client/#/";
             InitializeDriver();
             driver.Navigate().GoToUrl(url);
-            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
 
+            SelectElement list = new SelectElement(driver.FindElementByCssSelector("select"));
+            list.SelectByIndex(0);
             driver.FindElementById("usernameField").SendKeys(login);
             driver.FindElementById("passwordField").SendKeys(password + OpenQA.Selenium.Keys.Enter);
+        }
+
+        static void WriteCookiesToFile(string filePath)
+        {
+            FileInfo fileWriter = new FileInfo(filePath);
+            fileWriter.Create();
+           // StreamWriter fileWriter = new StreamWriter(File.Create(filePath));
+
+          //  if (fileWriter.)
+           // {
+                foreach (var cookie in driver.Manage().Cookies.AllCookies)
+                {
+                    //fileWriter.
+
+                }
+                //fileWriter
+            }
+
         }
     }
 }
